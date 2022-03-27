8a14528d97f91fa3c2de2201caafb93356182598
lfh <920298316@qq.com>
2022-03-27 22:11:10 +0800

    分支测试

--- src/main/java/org/lfh/mybatis/MybatisApplication.java
+++ src/main/java/org/lfh/mybatis/MybatisApplication.java
@@ -8,6 +8,7 @@ public class MybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
+        System.out.println("dev 分支的代码测试");
     }
 
 }
