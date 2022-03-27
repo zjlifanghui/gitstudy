2a1de58a5a4d96bf86f34ac5a0efc803d8ed4500
lfh <920298316@qq.com>
2022-03-27 22:20:49 +0800

    冲突测试001

--- src/main/java/org/lfh/mybatis/MybatisApplication.java
+++ src/main/java/org/lfh/mybatis/MybatisApplication.java
@@ -9,6 +9,8 @@ public class MybatisApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisApplication.class, args);
         System.out.println("dev 分支的代码测试");
+
+        System.out.println("冲突测试");
     }
 
 }
